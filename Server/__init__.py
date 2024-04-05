0eedbf878de8d74115a20ca17c471893f5e6e999
RogueManiac <70204504+RogueManiac@users.noreply.github.com>
2024-04-05 16:14:40 +0300

    Performance improvements

--- Server/__init__.py
+++ Server/__init__.py
@@ -33,70 +33,12 @@ lgr_model: Pipeline = Train.lgr_load()
 elr_model: Pipeline = Train.elr_load()
 
 
-def format_sse_msg(
-        ob0: Observation,
-        ob1: Observation,
-        pred: ndarray,
-        name: str,
-        logistic: bool
-) -> str:
-    """
-
-    Args:
-        ob0: Previous observation.
-        ob1: Current observation.
-        pred: Predicted state based on current observation.
-        name: Name of the model.
-        logistic: True for logistic learning.
-
-    Returns:
-        Message suitable for use in SSE.
-
-    """
-    global observation_q
-
-    # Data object
-    data: dict = {
-        name: {
-            "prev": {
-                "timestamp": str(ob0.timestamp),
-                "open": ob0.open,
-                "close": ob0.close,
-                "high": ob0.high,
-                "low": ob0.low,
-            },
-        }
-    }
-
-    # Data to be formatted to JSON
-    if logistic:
-        pred_val: float = float(pred)
-        direction: float = ob1.open + (1 if pred_val else -1)
-
-        data[name]["current"] = {
-            "timestamp": str(ob1.timestamp),
-            "open": ob1.open,
-            "p_close": direction,
-            "p_high": direction,
-            "p_low": direction
-        }
-    else:
-        data[name]["current"] = {
-            "timestamp": str(ob1.timestamp),
-            "open": ob1.open,
-            "p_close": pred[0][0],
-            "p_high": pred[0][1],
-            "p_low": pred[0][2]
-        }
-
-    return dumps(data)
-
-
 def run(
         name: str,
         pipeline: Pipeline,
         fed_rate: dict,
         q: Queue,
+        observation_q: Queue,
         logistic: bool = False,
 ) -> None:
     """
@@ -106,9 +48,64 @@ def run(
         pipeline: Model pipeline to use for prediction.
         fed_rate: Federal rate that is already in use.
         q: Queue that holds the multiprocessing output.
+        observation_q: Queue that holds prediction values.
         logistic: True for logistic learning.
 
     """
+
+    def format_sse_msg(
+            ob0: Observation,
+            ob1: Observation,
+            pred: ndarray,
+    ) -> str:
+        """
+
+        Args:
+            ob0: Previous observation.
+            ob1: Current observation.
+            pred: Predicted state based on current observation.
+
+        Returns:
+            Message suitable for use in SSE.
+
+        """
+        nonlocal name, logistic
+
+        # Data object
+        data: dict = {
+            name: {
+                "prev": {
+                    "timestamp": str(ob0.timestamp),
+                    "open": ob0.open,
+                    "close": ob0.close,
+                    "high": ob0.high,
+                    "low": ob0.low,
+                },
+            }
+        }
+
+        # Data to be formatted to JSON
+        if logistic:
+            pred_val: float = float(pred)
+            direction: float = ob1.open + (1 if pred_val else -1)
+
+            data[name]["current"] = {
+                "timestamp": str(ob1.timestamp),
+                "open": ob1.open,
+                "p_close": direction,
+                "p_high": direction,
+                "p_low": direction
+            }
+        else:
+            data[name]["current"] = {
+                "timestamp": str(ob1.timestamp),
+                "open": ob1.open,
+                "p_close": pred[0][0],
+                "p_high": pred[0][1],
+                "p_low": pred[0][2]
+            }
+
+        return dumps(data)
 
     # Loop indefinitely & push to queue
     while True:
@@ -134,7 +131,7 @@ def run(
 
         # yield the predicted close, high, low to the queue
         q.put_nowait(
-            format_sse_msg(prev_observation, cur_observation, y_pred, name, logistic)
+            format_sse_msg(prev_observation, cur_observation, y_pred)
         )
 
         # Time after execution
@@ -146,14 +143,14 @@ def run(
             sleep(abs(60 - t1 + t0))
 
 
-def total_observation(fed_rate: dict) -> None:
+def total_observation(q: Queue, fed_rate: dict) -> None:
     """
 
     Args:
+        q: Queue for output.
         fed_rate: Federal rate that is already in use.
 
     """
-    global observation_q
 
     # Loop indefinitely & push to queue
     while True:
@@ -169,7 +166,7 @@ def total_observation(fed_rate: dict) -> None:
 
         # Insert one observation for each model
         for i in range(3):
-            observation_q.put_nowait((prev_observation, cur_observation, sentiment))
+            q.put_nowait((prev_observation, cur_observation, sentiment))
 
         # Time after execution
         t1: time = time()
@@ -209,6 +206,7 @@ def start_model(
     pipeline: Pipeline,
     fed_rate: dict,
     q: Queue,
+    observation_q: Queue,
     logistic: bool = False,
 ) -> None:
     """
@@ -220,15 +218,16 @@ def start_model(
         pipeline: Model pipeline to use for prediction.
         fed_rate: Federal rate that is already in use.
         q: Queue that holds the multiprocessing output.
+        observation_q: Queue holding observations.
         logistic: True for logistic learning.
 
     """
     # Start the run cycle
-    run(name, pipeline, fed_rate, q, logistic)
+    run(name, pipeline, fed_rate, q, observation_q, logistic)
 
 
 def start() -> None:
-    global g_fed_rate, lr_q, lr_model, lgr_q, lgr_model, elr_q, elr_model
+    global g_fed_rate, lr_q, lr_model, lgr_q, lgr_model, elr_q, elr_model, observation_q
 
     options.display.max_columns = None
 
@@ -247,16 +246,16 @@ def start() -> None:
     fed_t.start()
 
     # Start the observation listener
-    Process(target=total_observation, args=(g_fed_rate,)).start()
+    Process(target=total_observation, args=(observation_q, g_fed_rate)).start()
 
     # Start the LR model process
-    Process(target=start_model, args=("lr", lr_model, g_fed_rate, lr_q, False)).start()
+    Process(target=start_model, args=("lr", lr_model, g_fed_rate, lr_q, observation_q, False)).start()
 
     # Start the LGR model process
-    Process(target=start_model, args=("lgr", lgr_model, g_fed_rate, lgr_q, True)).start()
+    Process(target=start_model, args=("lgr", lgr_model, g_fed_rate, lgr_q, observation_q, True)).start()
 
     # Start the ELR model process
-    Process(target=start_model, args=("elr", elr_model, g_fed_rate, elr_q, False)).start()
+    Process(target=start_model, args=("elr", elr_model, g_fed_rate, elr_q, observation_q, False)).start()
 
     # Do not use reloaded as it starts 2 separate processes (lots of headache)
     app.run(debug=True, port=config.server.port, use_reloader=False)
